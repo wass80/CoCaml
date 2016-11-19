990045d6926c20b2c3c21a3d6162bb0534e7c9c7
wass80 <wasss80@gmail.com>
2016-11-20 01:39:48 +0900

    better parser test

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -18,27 +18,28 @@ ptest p s x = case (Parsec.parse p "" s) of
   Left _ -> False
   Right ast -> ast == x
 
+testParser p s x = it s $ ptest p s x
+
 spec = do
   describe "parser" $ do
-    prop "empty" $ parse "" == Right []
+    it "empty" $ parse "" == Right []
   describe "prog" $ do
-    prop "empty" $ ptest prog "" []
+    it "empty" $ ptest prog "" []
   describe "token" $ do
-    prop "alpha" $ ptest token " aaa亜 " "aaa"
-    prop "文字" $ ptest token " 文字 " "文"
-    prop "文 - 字" $ ptest token " 文 - 字 " "文字"
-    prop "文-字 - 列 " $ ptest token " 文-字 - 列 " "文字列"
+    testParser token " aaa亜 " "aaa"
+    testParser token " 文字 " "文"
+    testParser token " 文 - 字 " "文字"
+    testParser token " 文-字 - 列 " "文字列"
   describe "idt" $ do
-    prop "文字" $ ptest idt " 文字 " (Idt "文")
+    testParser idt " 文字 " (Idt "文")
   describe "lidt" $ do
-    prop "文字" $ ptest lidt " 文字 " (LIdt $ Idt "文")
+    testParser lidt " 文字 " (LIdt $ Idt "文")
   describe "apply" $ do
-    prop "文" $ ptest apply " 文 " (LIdt $ Idt "文") 
-    prop "文字" $ ptest apply " 文字 "
+    testParser apply " 文 " (LIdt $ Idt "文")
+    testParser apply " 文字 "
       (Apply (LIdt $ Idt "文") (LIdt $ Idt "字"))
   describe "pipe" $ do
-    prop "文、字" $ ptest pipe " 文、字 "
+    testParser pipe " 文、字 "
       (Pipe (LIdt $ Idt "文") (LIdt $ Idt "字"))
-    prop "a 、f b" $ ptest pipe " a 、f b "
+    testParser pipe " a 、f b "
       (Pipe (LIdt $ Idt "a") (Apply (LIdt $ Idt "f") (LIdt $ Idt "b")))
-
