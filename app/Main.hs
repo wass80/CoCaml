867bd8d69c4af5275dcd66816a4e92c6976b9202
wass80 <wasss80@gmail.com>
2016-11-25 01:14:42 +0900

    update demo page

--- app/Main.hs
+++ app/Main.hs
@@ -12,24 +12,28 @@ import Transpiler (transpile)
 setText :: Elem -> String -> IO ()
 setText p = setProp p "textContent"
 
+update :: Elem -> Elem -> Elem -> Elem -> IO ()
+update input preview ast output = do
+  -- getValue :: (IsElem e, MonadIO m, JSType a) => e -> m (Maybe a)
+  (Just ln) <- getValue input
+  setText preview ln
+  case parse ln of
+    Right s -> do
+      setText ast (show s)
+      setText output (transpile s)
+    Left l -> do
+      setText ast (show l)
+      setText output "Error"
+
 main :: IO ()
 main = do
-    -- elemById ::  MonadIO m => ElemID -> m (Maybe Elem)
-    Just input <- elemById "input"
-    Just preview <- elemById "preview"
-    Just ast <- elemById "ast"
-    Just output <- elemById "output"
-    -- onEvent element event (EventData evt -> m ()) という形
-    -- この場合 e にはイベント情報が入り，（今回は使っていないが）函数に渡される
-    onEvent input KeyDown $ \ e -> do
-        -- getValue :: (IsElem e, MonadIO m, JSType a) => e -> m (Maybe a)
-        (Just ln) <- getValue input
-        setText preview ln
-        case parse ln of
-             Right s -> do
-               setText ast (show s)
-               setText output (transpile s)
-             Left l -> do
-               setText ast (show l)
-               setText output "Error"
-    return ()
+  -- elemById ::  MonadIO m => ElemID -> m (Maybe Elem)
+  Just input <- elemById "input"
+  Just preview <- elemById "preview"
+  Just ast <- elemById "ast"
+  Just output <- elemById "output"
+  update input preview ast output
+  -- onEvent element event (EventData evt -> m ()) という形
+  -- この場合 e にはイベント情報が入り，（今回は使っていないが）函数に渡される
+  onEvent input KeyDown $ \ e -> update input preview ast output
+  return ()
